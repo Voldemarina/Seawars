4d6862c69eab3a7dbd775c8c74fdbd4c4627ae74
Urbanovskiy Anton <antonurbanovskiy461@gmail.com>
2021-11-29 23:14:27 +0200

    edit api url

--- Seawars.Infrastructure/Data/ConnectionStrings.cs
+++ Seawars.Infrastructure/Data/ConnectionStrings.cs
@@ -14,7 +14,7 @@ namespace Seawars.Infrastructure.Data
         //    "Server=tcp:seabattle-databse.database.windows.net,1433;Initial Catalog=Seawars;Persist Security Info=False;User ID=AntonAdmin;Password=Anton2003;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
 
-        public const string ApiPath = "https://sea-wars.azurewebsites.net";
+        public const string ApiPath = "https://sea-wars.azurewebsites.net/";
 
         public const string MsSqlConnectionString = "Server=tcp:seawars-db-server.database.windows.net,1433;Initial Catalog=Seawars_db;User Id=Anton@seawars-db-server;Password=Vatazh0k";
 
