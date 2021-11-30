64437dd1d9c03aec1532e64fbb14bdf711b11c72
Urbanovskiy Anton <antonurbanovskiy461@gmail.com>
2021-11-30 22:43:06 +0200

    edit connection strings

--- Seawars.Infrastructure/Data/ConnectionStrings.cs
+++ Seawars.Infrastructure/Data/ConnectionStrings.cs
@@ -8,17 +8,10 @@ namespace Seawars.Infrastructure.Data
 {
     public static class ConnectionStrings
     {
-        //public const string ApiPath = "https://seawarswebapi.azurewebsites.net/";
-
-        //public const string MsSqlConnectionString =
-        //    "Server=tcp:seabattle-databse.database.windows.net,1433;Initial Catalog=Seawars;Persist Security Info=False;User ID=AntonAdmin;Password=Anton2003;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
 
         public const string ApiPath = "https://sea-wars.azurewebsites.net/";
 
         public const string MsSqlConnectionString = "Server=tcp:seawars-db-server.database.windows.net,1433;Initial Catalog=Seawars_db;User Id=Anton@seawars-db-server;Password=Vatazh0k";
-
-
 
     }
 }
